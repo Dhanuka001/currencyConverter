0552fb831032f7c22a21c7b3f0e1ec84e3607334
Dhanuka Rathnayaka <137430860+Dhanuka001@users.noreply.github.com>
2024-10-16 22:06:39 +0530

    added main page

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+        <h1>Convert Your Currencies.</h1>
+        <p>Effortlessly convert between world currencies with our easy-to-use currency converter. 
+            Stay up-to-date with the latest real-time exchange rates, ensuring accurate conversions 
+            for your travels, online shopping, or financial planning. Simply select your currencies, 
+            enter an amount, and get instant results. Whether you're a frequent traveler, 
+            business professional, or just curious about exchange rates, our tool makes 
+            currency conversion simple and reliable.</p>        
+    </div>
+  )
+}
